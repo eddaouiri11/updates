0db7582f57c8cb516bc268e4d5b98dcde158df85
eddaouiri11 <anaseddaouiri@gmail.com>
2026-03-31 16:29:57 +0100

    Update remote_version to 1.109.1

--- ver.php
+++ ver.php
@@ -14,7 +14,7 @@
   "space_required_in_GB": 0,
   "should_check_ab_load": false,
   "force_refresh_restype": "",
-  "remote_version": "1.109.5",
+  "remote_version": "1.109.1",
   "server_url": "https://loginbp.ggpolarbear.com/",
   "is_review_server": false,
   "use_login_optional_download": true,
