654413a42fba33e5df04acb24414d678925ee899
eddaouiri11 <anaseddaouiri@gmail.com>
2026-04-15 17:00:50 +0100

    Fix boolean values and update remote option version
    Updated boolean values and remote option version.

--- ver.php
+++ ver.php
@@ -1,22 +1,22 @@
 {
   "cdn_url": "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/",
-        "client_ip": "192.168.8.122",
-        "code": 0,
-        "country_code": "MA",
-        "force_to_restart_app": False,
-        "gdpr_version": 2,
-        "is_firewall_open": False,
-        "is_review_server": False,
-        "is_server_open": True,
-        "maintenance_announcement": "",
-        "maintenance_region": "",
-        "query_params": {
-            "appstore": "official",
-            "device": "android",
-            "lang": "ar",
-            "version": "1.15.3"
-        },
-        "remote_option_version": "1.0.0",
-        "remote_version": "1.15.3",
-        "server_url": "http://192.168.8.122:5000/"
+  "client_ip": "192.168.8.122",
+  "code": 0,
+  "country_code": "MA",
+  "force_to_restart_app": false,
+  "gdpr_version": 2,
+  "is_firewall_open": true,
+  "is_review_server": false,
+  "is_server_open": true,
+  "maintenance_announcement": "",
+  "maintenance_region": "",
+  "query_params": {
+    "appstore": "official",
+    "device": "android",
+    "lang": "ar",
+    "version": "1.15.3"
+  },
+  "remote_option_version": "1.15.3",
+  "remote_version": "1.15.3",
+  "server_url": "http://192.168.8.122:5000/"
 }
