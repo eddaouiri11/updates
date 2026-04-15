8735bc2268c4b2572822cb8e9cd51ce0739fdd05
eddaouiri11 <anaseddaouiri@gmail.com>
2026-04-15 16:57:07 +0100

    Update ver.php

--- ver.php
+++ ver.php
@@ -1,25 +1,22 @@
 {
-  "appstore_url": "http://play.google.com/store/apps/details?id=com.dts.freefireth",
-  "billboard_msg": "",
   "cdn_url": "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/",
-  "client_ip": "160.164.250.102",
-  "code": 0,
-  "country_code": "MA",
-  "force_to_restart_app": false,
-  "gdpr_version": 0,
-  "is_firewall_open": false,
-  "is_review_server": false,
-  "is_server_open": true,
-  "maintenance_announcement": "",
-  "maintenance_region": "",
-  "query_params": {
-    "appstore": "googleplay",
-    "device": "android",
-    "lang": "en",
-    "region": "DEFAULT",
-    "version": "1.15.3"
-  },
-  "remote_option_version": "1.0.0",
-  "remote_version": "1.15.3",
-  "server_url": "http://192.168.8.122:5000/"
+        "client_ip": "192.168.8.122",
+        "code": 0,
+        "country_code": "MA",
+        "force_to_restart_app": False,
+        "gdpr_version": 2,
+        "is_firewall_open": False,
+        "is_review_server": False,
+        "is_server_open": True,
+        "maintenance_announcement": "",
+        "maintenance_region": "",
+        "query_params": {
+            "appstore": "official",
+            "device": "android",
+            "lang": "ar",
+            "version": "1.15.3"
+        },
+        "remote_option_version": "1.0.0",
+        "remote_version": "1.15.3",
+        "server_url": "http://192.168.8.122:5000/"
 }
