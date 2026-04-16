6aadc65fce40be24238c1d9c39682b46bb308808
eddaouiri11 <anaseddaouiri@gmail.com>
2026-04-16 22:03:59 +0100

    Update ver.php

--- ver.php
+++ ver.php
@@ -1,25 +1,22 @@
 {
-  "appstore_url": "https://play.google.com/store/apps/details?id=com.dts.freefireth",
-  "billboard_msg": "",
-  "cdn_url": "https://loginbp.barbosasmobile.com/",
-  "client_ip": "197.128.236.124",
+  "cdn_url": "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/",
+  "client_ip": "192.168.8.122",
   "code": 0,
   "country_code": "MA",
   "force_to_restart_app": false,
   "gdpr_version": 2,
-  "is_firewall_open": false,
+  "is_firewall_open": true,
   "is_review_server": false,
   "is_server_open": true,
   "maintenance_announcement": "",
   "maintenance_region": "",
   "query_params": {
-    "appstore": "googleplay",
+    "appstore": "official",
     "device": "android",
-    "lang": "en",
-    "region": "DEFAULT",
-    "version": "1.17.1"
+    "lang": "ar",
+    "version": "1.15.3"
   },
-  "remote_option_version": "1.0.0",
-  "remote_version": "1.17.1",
-  "server_url": "https://loginbp.barbosasmobile.com/"
+  "remote_option_version": "1.15.3",
+  "remote_version": "1.15.3",
+  "server_url": "http://192.168.8.122:5000/"
 }
