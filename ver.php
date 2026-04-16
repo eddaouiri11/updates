4c610e30f6c5bf766777ff42af2a7667baf1d1a1
eddaouiri11 <anaseddaouiri@gmail.com>
2026-04-16 21:54:32 +0100

    Update ver.php

--- ver.php
+++ ver.php
@@ -1,22 +1,25 @@
 {
-  "cdn_url": "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/",
-  "client_ip": "192.168.8.122",
+  "appstore_url": "https://play.google.com/store/apps/details?id=com.dts.freefireth",
+  "billboard_msg": "",
+  "cdn_url": "https://loginbp.barbosasmobile.com/",
+  "client_ip": "197.128.236.124",
   "code": 0,
   "country_code": "MA",
   "force_to_restart_app": false,
   "gdpr_version": 2,
-  "is_firewall_open": true,
+  "is_firewall_open": false,
   "is_review_server": false,
   "is_server_open": true,
   "maintenance_announcement": "",
   "maintenance_region": "",
   "query_params": {
-    "appstore": "official",
+    "appstore": "googleplay",
     "device": "android",
-    "lang": "ar",
-    "version": "1.15.3"
+    "lang": "en",
+    "region": "DEFAULT",
+    "version": "1.17.1"
   },
   "remote_option_version": "1.0.0",
-  "remote_version": "1.15.3",
-  "server_url": "https://v0-server-edit-with-constants.vercel.app/api/game"
+  "remote_version": "1.17.1",
+  "server_url": "https://loginbp.barbosasmobile.com/"
 }
