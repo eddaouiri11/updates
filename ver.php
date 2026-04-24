e44cd95e2a16a88db03e0debc91e9ef84adf8fb1
eddaouiri11 <anaseddaouiri@gmail.com>
2026-04-24 10:21:04 +0100

    Update ver.php

--- ver.php
+++ ver.php
@@ -15,7 +15,7 @@
   "should_check_ab_load": false,
   "force_refresh_restype": "optionalavatarres",
   "remote_version": "1.123.7",
-  "server_url": "https://brevent.advance.freefiremobile.com/",
+  "server_url": "https://bdevent.ggpolarbear.com/",
   "is_review_server": false,
   "use_login_optional_download": true,
   "use_background_download": true,
