6f15a2ba7a4d46fa156bcadf3ecf0152d0a96c2a
eddaouiri11 <anaseddaouiri@gmail.com>
2026-04-25 09:23:10 +0100

    Update ver.php

--- ver.php
+++ ver.php
@@ -16,7 +16,7 @@
     "lang": "ar",
     "version": "1.15.3"
   },
-  "remote_option_version": "1.15.3",
+  "remote_option_version": "1.0.0",
   "remote_version": "1.15.3",
   "server_url": "http://192.168.8.122:5000/"
 }
