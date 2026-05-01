ec222bc117b62f9f88556ceeb21706739e358310
eddaouiri11 <anaseddaouiri@gmail.com>
2026-05-01 21:01:47 +0100

    Update ver.php

--- ver.php
+++ ver.php
@@ -14,7 +14,7 @@
   "space_required_in_GB": 1.48,
   "should_check_ab_load": false,
   "force_refresh_restype": "optionalavatarres",
-  "remote_version": "2.123.1",
+  "remote_version": "1.111.1",
   "server_url": "https://loginbp.ggpolarbear.com/",
   "is_review_server": false,
   "use_login_optional_download": true,
