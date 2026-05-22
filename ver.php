1219621b61e167ce3bc6cbacc3cedecc980cec77
eddaouiri11 <anaseddaouiri@gmail.com>
2026-05-22 23:24:36 +0100

    Update server URL in ver.php

--- ver.php
+++ ver.php
@@ -15,7 +15,7 @@
   "should_check_ab_load": false,
   "force_refresh_restype": "optionalavatarres",
   "remote_version": "1.123.15",
-  "server_url": "https://loginbp.ind.ggpolarbear.com/",
+  "server_url": "https://loginbp.ggblueshark.com/",
   "is_review_server": false,
   "use_login_optional_download": true,
   "use_background_download": false,
