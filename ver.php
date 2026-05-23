6801f1e2b0712554ef42834473818adb6149df5b
eddaouiri11 <anaseddaouiri@gmail.com>
2026-05-23 23:50:32 +0100

    Update server and client details in ver.php

--- ver.php
+++ ver.php
@@ -1,18 +1,22 @@
 {
-  "appstore_url": "http:/play.google.com/store/apps/details?id=com.dts.freefireth",
-  "billboard_msg": "",
-  "cdn_url": "https:/dl.cdn.freefiremobile.com/live/ABHotUpdates/",
-  "client_ip": "196.81.44.169",
+  "cdn_url": "https://dl.cdn.freefiremobile.com/live/ABHotUpdates/",
+  "client_ip": "192.168.8.122",
   "code": 0,
   "country_code": "MA",
   "force_to_restart_app": false,
-  "gdpr_version": 0,
-  "is_firewall_open": false,
+  "gdpr_version": 2,
+  "is_firewall_open": true,
   "is_review_server": false,
   "is_server_open": true,
   "maintenance_announcement": "",
   "maintenance_region": "",
-  "remote_option_version": "optionallocres:49|optionalavatarres:757|optionalclothres:1184|optionalfootballres:27|optionalfullscreencgres:319|optionalhuntinggroundres:246|optionalinfection:125|optionalingameres:503|optionallobbyres:613|optionallonewolfres:86|optionallonewolfstrikeoutres:59|optionalludores:42|optionalmap1res:385|optionalmap2res:156|optionalmap4res:139|optionalmaphippores:115|optionalmapres:357|optionalnewblast:163|optionalpetres:871|optionalrushb:108|optionalrushingpetsres:84|optionalsnowduelres:65|optionalsocialres:223|optionaltrainingres:297|optionalugcres:809|optionalvoiceres:341|optionalwerewolves:153|optionalwerunres:92|optionalmapponyres:204|optionalugcoldparadiseres:34|optionalmultiregionres:29",
+  "query_params": {
+    "appstore": "official",
+    "device": "android",
+    "lang": "ar",
+    "version": "1.24.0"
+  },
+  "remote_option_version": "1.0.0",
   "remote_version": "1.24.0",
-  "server_url": "https:/loginbp.ggpolarbear.com/"
+  "server_url": "http://192.168.8.122:5000/"
 }
