14f851b6684bc21e1fe9f5ffcab15d33a74fc945
eddaouiri11 <anaseddaouiri@gmail.com>
2026-07-26 21:44:14 +0100

    Update version details in ver.php

--- ver.php
+++ ver.php
@@ -1,6 +1,6 @@
 {
   "appstore_url": "https://play.google.com/store/apps/details?id=com.dts.freefireth",
-  "billboard_msg": "",
+  "billboard_msg": "Novo APK em breve",
   "cdn_url": "https://cdn.barbosasmobile.com/",
   "client_ip": "160.164.139.70",
   "code": 0,
@@ -9,10 +9,10 @@
   "gdpr_version": 2,
   "is_firewall_open": false,
   "is_review_server": false,
-  "is_server_open": true,
+  "is_server_open": false,
   "maintenance_announcement": "",
   "maintenance_region": "",
   "remote_option_version": "optionallocres:48|optionalavatarres:644|optionalclothres:72",
-  "remote_version": "1.25.3",
-  "server_url": "https://loginbp.barbosasmobile.com/"
+  "remote_version": "1.43.0",
+  "server_url": "https://loginbp2.barbosasmobile.com/"
 }
